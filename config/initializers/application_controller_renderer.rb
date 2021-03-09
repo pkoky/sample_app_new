85e80b08be69fa4e9a46dc649055d4070679680d
pkoky <sscsk2224@gmail.com>
2021-03-09 01:05:13 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dc81e8ca0409f6046308fa52da07a895eaa0a54bd16029caf8ad4a656e2794383ca03b9537ef72e88381caa8a414fbb83ec978819e9093638b4d752678bb413b'
